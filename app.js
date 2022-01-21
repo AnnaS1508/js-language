7302fe9e3d0ccb1375525e0c4d7f97d33bb7881e
AnnaSvirskaya <svirskaya1983@list.ru>
2022-01-21 10:21:55 +0200

    js_4_foreach_map-filters_reduce

--- app.js
+++ app.js
@@ -1,50 +1,48 @@
-console.log("Hello World");
-/*for(var i=0;i<3;i++)
-setTimeout(function(){
-    console.log(i);
-})*/
-
-/*for(let i=0;i<3;i++){
-    setTimeout(function(){
-        console.log(i);
-    })
-}*/
-
-const fun = function(a, b){
-    return a + b;
+function mapper (n, i, a) {
+  return `<li>${i + 1} of ${a.length} - ${n}</li>`    
 }
-let a=5;
-//console.log(a**2);
-//console.log(fun(10,20));
-//console.log(a(10,20));error
-//console.log(fun**2);NaN
-//console.log("12"+12);
-//console.log("12"-12);
-//console.log("ab"-12);
-//console.log(`+"12"+12 = $ {+"12"+12}`)
-
-//HOMEWORK:
-console.log(("a" + + "a" +'a'+'s').toLowerCase());
-
-
-
-function calc(num1,num2,num3) {
-
-    if(num2 =="+"){
-        return num1 + num3;}
-else if (num2 =="-"){
-    return num1 - num3;} 
-else if (num2 == "*"){
-    return num1 * num3;}
-else if(num2 =="/"){
-    return num1/num3;}
+function someFunction() {
+   const ar = [-10, 50, -12, 80, 90];
+   const ar3 = ar.map(mapper);
+console.log(ar3);
 
 }
-console.log(calc(10,'+',5));
+const ar = [-10, 50, -12, 80, 40];
+ar.push(70);
+// str - "-10#50#-12#80#40#70"
+/*********solution based on substring */
+// let str = '';
+// ar.forEach(function(n) {
+//         str += n + '#';
+// })
+// str = str.substring(0, str.length - 1);
+/*************************solution based on forEach form second number */
+// const ar1 = ar.slice(1);
+// let str =  '' + ar[0];
+// ar1.forEach(n => str += '#' + n);
+// console.log(str);
+/*****************************printing out sequense number of element, element, length of arry */
+// 1 of 5 - -10;    ....
+ar.forEach((n, i, a) => console.log(`${i + 1} of ${a.length} - ${n}`));
 
+// method "map"
+//use case of applying method map : you want to create new array with elements that are received as result of some conversion
+//example you want to get new array with elements that are muliplication on 2 of each source element
+const ar2 = ar.map(n => n * 2);
+console.log(ar2);
+someFunction();
+/*****************HW 13 task1 definition */
+// write function myForEach(array, callback-function);
+//array  - being iterated array
+//callback-function - function that will be called for each element of array
+//callback-function should take three arguments: current elemnt, current index, being iterated array
+//example of standard forEach : array.forEach(n => str += '#' + n);
+//example of myForEach : myForEach(array, n => str += '#' + n);
 
+/********************************************************************* */
+/**********************************************HW 13 definition task2 */
+//write method myMap  for the same functionality as standard method map
+//function myMap(array, callback-function)
+//myMap will apply your method myForEach
+  
 
-function sum(a){
-    return (b) => a + b
-}
-console.log(sum(10,3))
